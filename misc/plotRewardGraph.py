33e7603e79c6785663188330a0c9e27cc8ce94d0
uzairakbar <uzair.akbar@tum.de>
2019-07-13 05:06:06 +0200

    Update plotRewardGraph.py

--- misc/plotRewardGraph.py
+++ misc/plotRewardGraph.py
@@ -1,6 +1,9 @@
-# Load libraries
+#! /usr/bin/env python
 import sys
 import numpy as np
+import matplotlib
+# in case matplotlib backend isn't working, uncomment this:
+# matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
@@ -16,25 +19,32 @@ colors = cm.rainbow(np.linspace(0, 1, len(sys.argv[3:])))
 fig = plt.figure()
 
 for i, base_filename in enumerate(sys.argv[3:]):
-	rewards = np.zeros((num_episodes, num_simulations))
+    rewards = np.zeros((num_episodes, num_simulations))
 
-	for sim_num in range(num_simulations):
-		filename = "trainingRewardLog_" + str(sim_num) + base_filename + ".npy"
-		rewards[:, sim_num] = np.load(filename)[:num_episodes]
+    for sim_num in range(num_simulations):
+        filename = base_filename+"_sim"+str(sim_num)+".npy"
+        rewards[:, sim_num] = np.load(filename)[:num_episodes]
 
-	# Create means and standard deviations of rewards
-	rewards_mean = np.mean(rewards, axis=1)
-	rewards_std = np.std(rewards, axis=1)
+    # Create means and standard deviations of rewards
+    rewards_mean = np.mean(rewards, axis=1)
+    rewards_std = np.std(rewards, axis=1)
 
-	plt.plot(episode_array, rewards_mean, '--', color=colors[i],  label=base_filename)
+    plt.plot(episode_array, rewards_mean, '--', color=colors[i],  label=base_filename)
 
-	# Draw bands
-	plt.fill_between(episode_array, rewards_mean - rewards_std, rewards_mean + rewards_std, color=colors[i], alpha = 0.5)
+    # Draw bands
+    plt.fill_between(episode_array, rewards_mean - rewards_std, rewards_mean + rewards_std, color=colors[i], alpha = 0.5)
 
 # Create plot
-# plt.title("Learning Curve")
+plt.title("Cumulative Episode Rewards")
 plt.xlabel("Number of Episodes"), plt.ylabel("Episode Reward")
+plt.xlim([0., num_episodes])
+# uncomment if log scale required:
+# plt.yscale('log')
+plt.legend(sys.argv[3:])
+# provide custom legend:
+# plt.legend(['plt1', 'plt2'])
 plt.tight_layout()
+plt.grid()
 plt.show()
 
-fig.savefig("plotRewardGraph", format="pdf", dpi=1200)
+fig.savefig("plotRewardGraph.pdf", format="pdf", dpi=1200)
